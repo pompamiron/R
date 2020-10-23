bdc0f58a3281ef4f2817bb8898ad35ebe104e3d1
Chalakorn <71726498+pompamiron@users.noreply.github.com>
2020-10-23 07:03:16 +0700

    Add files via upload

---
+++ LAB-R/1_oddcount.r
@@ -0,0 +1,9 @@
+# counts the number of odd integers in x 
+oddcount <- function(x) {
+  k <- 0 # assign 0 to k 
+  for (n in x) {
+    if (n %% 2 == 1) k <- k+1 
+    # %% is the modulo operator 
+} 
+return(k) 
+}
